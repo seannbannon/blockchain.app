7d5b8bc58119654f57ee9022f9ae13ec72590f46
Sean Bannon <seannbannon@gmail.com>
2021-08-18 16:25:17 -0500

    learning truffle and adding dependencies to 2nd migration and woking on tokenfarm smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,5 @@
+const DappToken = artifacts.require('DappToken')
+const DaiToken = artifacts.require('DaiToken')
 const TokenFarm = artifacts.require('TokenFarm')
 
 module.exports = function(deployer) {
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,7 +1,16 @@
 pragma solidity ^0.5.16;
 
-contract TokenFarm{
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
+contract TokenFarm {
     // All code goes here...
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
 
+     constructor(DappToken _dappToken, DaiToken _daiToken) public {
+         dappToken = _dappToken;
+         daiToken = _daiToken;
+     }
 }
