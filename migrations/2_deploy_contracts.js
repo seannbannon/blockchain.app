f76b65f6ed37e227df682d4a653db2261483f8ab
Sean Bannon <seannbannon@gmail.com>
2021-08-18 16:32:19 -0500

    deploying tokens to blockchain

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,16 @@ const DappToken = artifacts.require('DappToken')
 const DaiToken = artifacts.require('DaiToken')
 const TokenFarm = artifacts.require('TokenFarm')
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm)
+module.exports = async function(deployer, network, accounts) {
+    //Deploy Mock DAI Token
+    await deployer.deploy(DaiToken)
+    const daiToken = await DaiToken.deployed()
+
+    //Deploy DAPP Token
+    await deployer.deploy(DappToken)
+    const dappToken = await DappToken.deployed()
+
+    //  Deploy TokenFarm
+    await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+    const tokenFarm = await TokenFarm.deployed()
 }
