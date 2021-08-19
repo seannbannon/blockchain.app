bfe5365c8a58ba6c32d056789ef8cafb3a1f44fc
Sean Bannon <seannbannon@gmail.com>
2021-08-19 17:31:55 -0500

    transfering tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -14,4 +14,10 @@ module.exports = async function(deployer, network, accounts) {
     //  Deploy TokenFarm
     await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
     const tokenFarm = await TokenFarm.deployed()
+
+    // Transfer all tokens to TokenFarm (a milli)
+    await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+    // Transfer 100 Mock DAI tokens to investor
+    await daiToken.transfer(accounts[1], '100000000000000000000')
 }
