9a90e42911eba59bcc57bb57890b41a92ceaba80
Sean Bannon <seannbannon@gmail.com>
2021-08-20 13:18:01 -0500

    all tests pass

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -2,6 +2,9 @@
 // const { Item } = require('react-bootstrap/lib/Breadcrumb')
 // const _deploy_contracts = require('../migrations/2_deploy_contracts')
 
+const { assert } = require('chai')
+const { default: Web3 } = require('web3')
+
 const DaiToken = artifacts.require('DaiToken')
 const DappToken = artifacts.require('DappToken')
 const TokenFarm = artifacts.require('TokenFarm')
@@ -11,14 +14,50 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
+  function tokens(n){
+      return web3.utils.toWei(n, 'ether');
+  }
 
-contract('TokenFarm', (accounts) => {
+contract('TokenFarm', ([owner, investor]) => {
     // write tests here
+    let daiToken, dappToken, tokenFarm
+
+    before(async () => {
+        // load contracts
+        daiToken = await DaiToken.new()
+        dappToken = await DappToken.new()
+        tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+        // transfer all Dapp tokens to farm (1 milli)
+        await dappToken.transfer(tokenFarm.address, tokens('1000000'))
+
+        // send tokens to investor
+        await daiToken.transfer(investor, tokens('100'), { from: owner })
+    })
+
     describe('Mock DAI deployment', async () => {
         it('has a name', async () => {
-           let daiToken = await DaiToken.new()
            const name = await daiToken.name()
            assert.equal(name, 'Mock DAI Token')
+        })
+    })
+
+    describe('Dapp Token deployment', async () => {
+        it('has a name', async () => {
+           const name = await dappToken.name()
+           assert.equal(name, 'DApp Token')
+        })
+    })
+
+    describe('Token Farm deployment', async () => {
+        it('has a name', async () => {
+           const name = await tokenFarm.name()
+           assert.equal(name, 'Dapp Token Farm')
+        })
+
+        it('contract has tokens', async () => {
+            let balance = await dappToken.balanceOf(tokenFarm.address)
+            assert.equal(balance.toString(), tokens('1000000'))
         })
     })
 })
