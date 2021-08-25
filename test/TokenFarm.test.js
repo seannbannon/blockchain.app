0bfc56ac02823b6303c8eeb760bc47e968abb67d
Sean Bannon <seannbannon@gmail.com>
2021-08-25 08:46:00 -0500

    stake mock dai

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -69,7 +69,12 @@ contract('TokenFarm', ([owner, investor]) => {
             //Check investor balance before staking
             result = await daiToken.balanceOf(investor)
             assert.equal(result.toString(), tokens('100'), 'investor Mock DAI wallet balance correct before staking')
+
+            //Stake Mock DAI Tokens
+            await daiToken.approve(tokenFarm.address, tokens('100'), {from: investor})
+            await tokenFarm.stakeTokens(tokens('100'), { from: investor })
         })
+        
 
     })
 })
